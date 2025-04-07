1c781be037be7a92d9321953bb370067f9fe53ae
Josh Omer <omerbiz.josh@gmail.com>
2025-04-07 12:40:02 -0600

    Fixing bugs

--- main.py
+++ main.py
@@ -51,6 +51,7 @@ def get_cpu():
     else:
         query = "SELECT * FROM CPU"
     '''
+    brand: str = "-- Brands --" 
     brand = request.args.get("brands")
     price_sort = request.args.get("price_sort")
     query = "SELECT * FROM CPU"
